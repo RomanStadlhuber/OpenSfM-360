f9f37779828b19c4c3197716c000004653504595
Roman Stadlhuber <r.stadlhuber@gmail.com>
2025-05-27 17:10:39 +0200

    add ability to use arbitrary video scale (overwrites camera model JSON) and specify relative start and end times of video also, remove the dataset dir if it exists to avoid duplicate frames increase standard video stride (about every 3 seconds for ~30 FPS)

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
-if [ "$#" -lt 2 ] || [ "$#" -gt 3 ]; then
-    echo "Usage: $0 INPUT_PATH DATA_DIR [VIDEO_STRIDE]"
+if [ "$#" -lt 2 ]; then
+    echo "Usage: $0 INPUT_PATH DATA_DIR [VIDEO_STRIDE] [0 <= START <= 1] [start < END <= 1]"
     exit 1
 fi
 
 INPUT_PATH="$1"
 DATA_DIR="$2"
 VIDEO_STRIDE="${3:-30}"
+START="${4:-0}"
+END="${5:-1}"
 
 # convert video to dataset
 cd /source/OpenSfM/app
 echo "creating dataset from video"
-uv run main.py "$INPUT_PATH" "$DATA_DIR" --video_stride "$VIDEO_STRIDE" convert_video
+uv run main.py "$INPUT_PATH" "$DATA_DIR" --video_stride "$VIDEO_STRIDE" --start "$START" --end "$END"
 echo "dataset created"
 cd /source/OpenSfM
 echo "starting reconstruction"
--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@
 from __future__ import annotations
 from jsonargparse import auto_cli
 from pathlib import Path
-import subprocess
+import shutil
 import ffmpeg
 import yaml
 import json
@@ -15,50 +15,87 @@ CAMERA_MODEL_JSON = {  # common camera model used for all 360 images
         "height": 960,
     }
 }
-# from: https://www.dropbox.com/scl/fo/3q1m88yp3mcsb6kg0hez8/AFSGDg_7Rr5X3INg2mKFUsk?dl=0&e=1&preview=config.yaml&rlkey=h6p1g8b0pcnn3uwu1s5uvnald
+# from: https://www.dropbox.com/scl/fo/3q1m88yp3mcsb6kg0hez8/AFSGDg_7Rr5X3INg2mKFUsk?dl=0&e=1&preview=config.yaml
 CONFIG_YAML = {
-    "feature_process_size": 1024,    # Resize the image if its size is larger than specified. Set to -1 for original size
-    "feature_min_frames": 16000,     # If fewer frames are detected, sift_peak_threshold/surf_hessian_threshold is reduced.
-    "processes": 8,                  # Number of threads to use
+    "feature_process_size": 1024,  # Resize the image if its size is larger than specified. Set to -1 for original size
+    "feature_min_frames": 16000,  # If fewer frames are detected, sift_peak_threshold/surf_hessian_threshold is reduced.
+    "processes": 8,  # Number of threads to use
 }
 
 
-class Main:
-    def __init__(self, input_path: str, dataset_dir: str, video_stride: int = 30):
-        self.input_path = Path(input_path)
-        self.dataset_dir = Path(dataset_dir)
-        self.stride = video_stride
-        pass
+def convert_video(
+    input_path: str,
+    dataset_dir: str,
+    video_stride: int = 180,
+    start: float = 0,
+    end: float = 1.0,
+    override_video_dimensions: bool = True,
+) -> None:
+    """Converts a 360° video into an OpenSfM dataset."""
+    # clip start and end to [0;1]
+    start = min(0, max(start, 1))
+    end = min(1, max(end, 0))
+    assert start < end, "Start time must be less than end time."
+    dataset_dir = Path(dataset_dir)
+    # remove existing dataset directory if it exists
+    if dataset_dir.exists():
+        shutil.rmtree(dataset_dir)
+    print("create dataset folder")
+    dataset_dir.mkdir(parents=True, exist_ok=True)
+    print("create image folder")
+    image_dir = dataset_dir / "images"
+    image_dir.mkdir(parents=True, exist_ok=True)
+    # load video info
+    probe = ffmpeg.probe(input_path)
+    if override_video_dimensions:
+        width = probe["streams"][0]["width"]
+        height = probe["streams"][0]["height"]
+        print(f"Overriding camera model dimensions to {width}x{height}")
+        camera_model_overrides = CAMERA_MODEL_JSON.copy()
+        camera_model_overrides["all"]["width"] = width
+        camera_model_overrides["all"]["height"] = height
+        json.dump(camera_model_overrides, open(dataset_dir / "camera_models_overrides.json", "w"), indent=4)
+    else:  # use default dimensions
+        width = 1920
+        height = 960
+        json.dump(CAMERA_MODEL_JSON, open(dataset_dir / "camera_models_overrides.json", "w"), indent=4)
+    # find absolute start and end time
+    try:
+        start = start * float(probe["streams"][0]["start_time"])
+        end = end * float(probe["streams"][0]["duration"])
+        t = end - start  # "-t" determines the trim duration after the start time
+    except Exception as e:
+        raise RuntimeError(f"Error parsing video info from ffprobe: {e}")
 
-    def convert_video(self) -> None:
-        """Converts a 360° video into an OpenSfM dataset."""
-        print("create dataset folder")
-        self.dataset_dir.mkdir(parents=True, exist_ok=True)
-        print("create image folder")
-        image_dir = self.dataset_dir  / "images"
-        image_dir.mkdir(parents=True, exist_ok=True)
-        print("creating config files")
-        json.dump(CAMERA_MODEL_JSON, open(self.dataset_dir  / "camera_models_overrides.json", "w"), indent=4)
-        yaml.dump(CONFIG_YAML, open(self.dataset_dir  / "config.yaml", "w"))
-        print("exporting frames")
-        filter_expr = f"select=not(mod(n\\,{self.stride}))"
-        (ffmpeg
-            .input(self.input_path)
-            .output(
+    print("creating config files")
+    yaml.dump(CONFIG_YAML, open(dataset_dir / "config.yaml", "w"))
+    filter_expr = f"select=not(mod(n\\,{video_stride}))"
+
+    # Format start time as hh:mm:ss
+    def seconds_to_hms(seconds):
+        hours = int(seconds // 3600)
+        minutes = int((seconds % 3600) // 60)
+        secs = seconds % 60
+        return f"{hours:02d}:{minutes:02d}:{secs:06.3f}"
+
+    start_hms = seconds_to_hms(start)
+    t_hms = seconds_to_hms(t)
+
+    print(f"exporting frames from -ss {start_hms}, -t {t_hms}")
+
+    (
+        ffmpeg.input(input_path, ss=start_hms, t=t_hms)
+        .output(
             str(image_dir / "frame_%04d.png"),
-            vf=f"{filter_expr},scale=1920:960",
+            vf=f"{filter_expr},scale={width}:{height}",
             vsync="vfr",
-            qscale=2
-            )
-            .run()
+            qscale=2,
         )
-        num_frames = len(list(image_dir.glob("*.png")))
-        print(f"Exported {num_frames} frames to {image_dir}")
-
-    ## # TODO: in the future, generate a dataset from directory of (unordered) images
-    ## def convert_images(self) -> None:
-    ##     raise NotImplementedError("TODO: convert (unordered) images to OpenSfM dataset")
+        .run()
+    )
+    num_frames = len(list(image_dir.glob("*.png")))
+    print(f"Exported {num_frames} frames to {image_dir}")
 
 
 if __name__ == "__main__":
-    auto_cli(Main, description="Generate OpenSfM datasets from 360° videos and process them.")
+    auto_cli(convert_video)
